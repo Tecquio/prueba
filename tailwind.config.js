1f30a27b615a107b23789fddf099599d130a6213
yairsinecio <sinecioyair@gmail.com>
2025-06-22 12:45:28 -0600

    Arreglo final: Se ajustan versiones de Tailwind y se completa el diseño

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Esto le dice a Tailwind que busque clases en todos tus archivos de React.
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      // Aquí puedes añadir tus colores si quieres darles nombres personalizados
-      // para no tener que escribir los códigos hexadecimales todo el tiempo.
-      // Ejemplo:
-      // colors: {
-      //   'primary-green': '#00B8A9',
-      //   'primary-red': '#F94144',
-      // }
-    },
+    extend: {},
   },
   plugins: [],
 }
